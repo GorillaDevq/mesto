4d8e2fcbc5d6b7b950a96f785dba5a0f63bcddf7
Gorilla Dev <gorilladevq@gmail.com>
2022-11-15 18:29:32 +0300

    feaching popup

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let openPopup = document.querySelector(".button__edit");
+let popup = document.querySelector(".popup");
+
+openPopup.addEventListener('click', function (){
+  popup.classList.remove("popup_hidden");
+  console.log("asdasdsad");
+});
