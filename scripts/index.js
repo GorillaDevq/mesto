3738ad0c5be37261a84ecfa4259828af2a254e6b
Gorilla Dev <gorilladevq@gmail.com>
2022-11-17 12:27:30 +0300

    first iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,31 @@
-let openPopup = document.querySelector(".button__edit");
-let popup = document.querySelector(".popup");
+//Открыть-закрыть попап
+let openPopup = document.querySelector('.button__edit');
+let popup = document.querySelector('.popup');
+let closePopup = document.querySelector('.popup__close');
+function close(){
+  popup.classList.remove('popup_opened');
+};
 
 openPopup.addEventListener('click', function (){
-  popup.classList.remove("popup_hidden");
-  console.log("asdasdsad");
+  professionUserInput.value = professionUser.textContent;
+  nameUserInput.value = nameUser.textContent;
+  popup.classList.add('popup_opened');
+});
+closePopup.addEventListener('click', close);
+// Submit popap
+let submitPopup = document.querySelector('.popup__submit');
+let nameUser = document.querySelector('.profile__name');
+let nameUserInput = document.querySelector('.popup__input_type_name');
+let professionUser = document.querySelector('.profile__profession');
+let professionUserInput = document.querySelector('.popup__input_type_profession');
+
+
+submitPopup.addEventListener('click', function (evt){
+  evt.preventDefault();
+  if (nameUserInput.value === '' || professionUserInput.value === ''){
+  } else {
+    nameUser.textContent = nameUserInput.value;
+    professionUser.textContent = professionUserInput.value;
+    close();
+  };
 });
